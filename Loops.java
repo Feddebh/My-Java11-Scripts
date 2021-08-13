b60e88b397755c54b13537a297a2cc1ee9fe6ba3
Feddebh <bluisfederico@gmail.com>
2021-08-13 00:49:43 -0300

    Added for andforeach loops examples AUG132021

--- Loops.java
+++ Loops.java
@@ -6,5 +6,19 @@ public class Loops {
         while (i < 5){
             System.out.println(i); i++;
         }
+
+        //for loop
+        //When you know exactly how many times you want to loop through
+        // a block of code, use the for loop instead of a while loop:
+        for (int a = 0; a < 5; a++) {
+            System.out.println(a);
+        }
+
+        //The forEach loop
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String x : cars) {
+            System.out.println(x);
+        }
     }
 }
